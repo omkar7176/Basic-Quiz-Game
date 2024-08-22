2960d970e1f6de75e552132cc4568a82eaf380ec
Omkar Tupere <115610759+omkar7176@users.noreply.github.com>
2024-08-22 16:52:04 +0000

    new file added

---
+++ quiz_data.py
@@ -0,0 +1,23 @@
+# Define the quiz questions, options, and correct answers
+quiz_data = [
+    {
+        "question": "Which language is widely use to analyze the data?",
+        "options": ["Go", "JavaScript", "Python", "Java"],
+        "correct": 2
+    },
+    {
+        "question": "What is the capital of France?",
+        "options": ["Paris", "London", "Berlin", "Rome"],
+        "correct": 0
+    },
+    {
+        "question": "What is the largest planet in our solar system?",
+        "options": ["Earth", "Saturn", "Jupiter", "Uranus"],
+        "correct": 2
+    },
+    {
+        "question": "What is the smallest country in the world?",
+        "options": ["Vatican City", "Monaco", "Nauru", "Tuvalu"],
+        "correct": 0
+    }
+]
